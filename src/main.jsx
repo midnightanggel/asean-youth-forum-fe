522f7abab7718c312b2dc537ad16e2128ea9a984
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-05 14:33:16 +0700

    improvement: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <>
+      <h1 className="text-5xl font-bold">Hello </h1>
+    </>
+  </StrictMode>
+);
