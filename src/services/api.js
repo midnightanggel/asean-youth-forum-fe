7499f94d014a61fe5db36aeefedddcd6c9893a42
sugiartofahmi <sugiartofahmi@gmail.com>
2023-06-13 23:27:16 +0700

    feature: register

--- src/services/api.js
+++ src/services/api.js
@@ -7,36 +7,57 @@ const api = axios.create({
   },
 });
 
-export const get = async (url, params) => {
+export const get = async (url, params = {}, token) => {
   try {
-    const response = await api.get(url, { params });
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params,
+    };
+    const response = await api.get(url, config);
     return response.data;
   } catch (error) {
     console.error("Error:", error.message);
   }
 };
 
-export const post = async (url, data) => {
+export const post = async (url, data, token) => {
   try {
-    const response = await api.post(url, data);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.post(url, data, config);
     return response.data;
   } catch (error) {
     console.error("Error:", error.message);
   }
 };
 
-export const put = async (url, data) => {
+export const put = async (url, data, token) => {
   try {
-    const response = await api.put(url, data);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.put(url, data, config);
     return response.data;
   } catch (error) {
     console.error("Error:", error.message);
   }
 };
 
-export const remove = async (url) => {
+export const remove = async (url, token) => {
   try {
-    const response = await api.delete(url);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await api.delete(url, config);
     return response.data;
   } catch (error) {
     console.error("Error:", error.message);
